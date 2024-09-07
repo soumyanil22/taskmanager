d86530e5050b8cd9b044bdb30d71536d2eff0cd3
Soumyanil Podder <soumyanil22@gmail.com>
2024-09-07 14:16:35 +0530

    refactor: user service adjusted

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,23 +1,5 @@
 const UserModel = require('../models/User');
 
-const loginUser = async (email, password) => {
-    const user = await UserModel.findOne({ email, password });
-    return user;
-}
-
-const registerUser = async (name, email, password) => {
-    const existingUser = await UserModel.findOne({ email });
-
-    if (existingUser) {
-        throw new Error('User already exists');
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 8);
-    const newUser = await UserModel.create({ name, email, password: hashedPassword });
-
-    return newUser;
-}
-
 const getUser = async (id, email) => {
     if (!id || !email) {
         throw new Error('Please provide id or email');
