2c21e5e3bced46700dda09c5bfdaedaf3f720979
Soumyanil Podder <soumyanil22@gmail.com>
2024-09-10 04:13:07 +0530

    refactor: added logs

--- backend/middlewares/Authentication.js
+++ backend/middlewares/Authentication.js
@@ -1,6 +1,8 @@
 // a middleware that checks if the user is authenticated
 
 const ensureAuthenticated = (req, res, next) => {
+    console.log("SESSION =>", req.session);
+    console.log("AUTH =>", req.isAuthenticated());
     if (req.isAuthenticated()) {
         return next();
     }
