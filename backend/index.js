44cf98b6f048064a929c33eb58183e4a8a606c84
Soumyanil Podder <soumyanil22@gmail.com>
2024-09-09 18:59:35 +0530

    fix: cookie config fixes

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false, cookie: {
         httpOnly: true, // Prevents client-side JS from accessing the cookie
         sameSite: 'none',
-        secure: true,
+        secure: process.env.NODE_ENV === 'production',
         maxAge: 1000 * 60 * 60 * 24 // Session expires after 24 hours
     }
 }));
