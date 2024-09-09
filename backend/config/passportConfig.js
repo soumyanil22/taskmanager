fefd390c1d8a3f488c86c80eed397e44a560b236
Soumyanil Podder <soumyanil22@gmail.com>
2024-09-09 17:14:37 +0530

    fix: google oauth conf

--- backend/config/passportConfig.js
+++ backend/config/passportConfig.js
@@ -29,7 +29,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: '/auth/google/callback',
+            callbackURL: 'https://sparkling-douhua-a6483b.netlify.app/auth/google/callback',
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
