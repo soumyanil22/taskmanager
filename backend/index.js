efbc43d076553ba940441e03c0f926b6569509f6
Soumyanil Podder <soumyanil22@gmail.com>
2024-09-09 19:09:45 +0530

    fix: session fixes

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const passport = require("passport");
 const session = require("express-session");
+const RedisStore = require('connect-redis')(session);
+const redisClient = require('redis').createClient();
 const ensureAuthenticated = require("./middlewares/Authentication");
 const connection = require("./config/db");
 require("dotenv").config();
@@ -13,6 +15,7 @@ const app = express();
 app.use(cors({ origin: ["http://localhost:5173,", "https://sparkling-douhua-a6483b.netlify.app"], credentials: true }));
 app.use(express.json());
 app.use(session({
+    store: new RedisStore({ client: redisClient }),
     secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false, cookie: {
         httpOnly: true, // Prevents client-side JS from accessing the cookie
         sameSite: 'none',
