03871f55f7879104fb6c6286761ba0a42f02cf04
Soumyanil Podder <soumyanil22@gmail.com>
2024-09-09 21:55:09 +0530

    refactor: axios config

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
+axios.defaults.withCredentials = true;
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
-    withCredentials: true
 })
 
 
